565bd23b5cb9204413fc1eef9f41c42b4b79876a
lsimonetti <44656505+lsimonetti@users.noreply.github.com>
2024-04-15 10:05:57 -0300

    Update Projeto 1 IC.py

--- Projeto 1 IC.py
+++ Projeto 1 IC.py
@@ -3,17 +3,9 @@
 import cplex
 from Leitor import faz_tudo
 
-
-
-grafo = (faz_tudo(open(r'C:\Users\Gio Faletti\Documents\GioPosEscola\ic\codigos\ProgVS\grafo.txt')))
-#colocar o endereço do grafo.txt
-vertices = len(grafo)
-#as arestas sao 1 ou 0(existem ou nao) em grafo[1no][2no]
-#Quero que haja pares de caminhos que separem todas as arestas, quantidade de pares de caminhos para que todas as combinacoes 2 a 2 de arestas sejam separadas é a psn(G)
-def setupproblem(c, grafo, nedge):
+def setupproblem(grafo, nedge, nvert):
     cpx = cplex.Cplex()
     
-    nvert = len(grafo)
     lim_cam = 19*nvert
 
     e = 0
@@ -59,3 +51,11 @@ def setupproblem(c, grafo, nedge):
   #                for i in range(vertices)],
    #    G, L, E, R(anged) senses=[] * vertices,
     #  DIREITA  rhs=[0.0] * vertices)
+
+
+grafo, nedge = (faz_tudo(open(r'C:\Users\Gio Faletti\Documents\GioPosEscola\ic\codigos\ProgVS\grafo.txt')))
+#colocar o endereço do grafo.txt
+vertices = len(grafo)
+#as arestas sao 1 ou 0(existem ou nao) em grafo[1no][2no]
+#Quero que haja pares de caminhos que separem todas as arestas, quantidade de pares de caminhos para que todas as combinacoes 2 a 2 de arestas sejam separadas é a psn(G)
+setupproblem(grafo, nedge, vertices)
