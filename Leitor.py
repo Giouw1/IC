cb8da7d4009afc77b63e8474bb4ff58abf4e4891
Giovanni Faletti Almeida <130237551+Giouw1@users.noreply.github.com>
2024-08-12 12:54:38 -0300

    Update Leitor.py

--- Leitor.py
+++ Leitor.py
@@ -3,15 +3,15 @@ def matriz_adj(texto_lido):
     for k in range(texto_lido[0][0]):#quantidade de vertices
         matriz_f += [[0]*texto_lido[0][0]]
     for i in range(1,len(texto_lido)):
-        matriz_f[texto_lido[i][0]-1][texto_lido[i][1]-1] = 1
-        matriz_f[texto_lido[i][1]-1][texto_lido[i][0]-1] = 1
+        matriz_f[texto_lido[i][0]][texto_lido[i][1]] = 1
+        matriz_f[texto_lido[i][1]][texto_lido[i][0]] = 1
     return matriz_f
 def text_reader(texto):
     auxiliar = []
     marcador = 0
     grafo= []
     for i in (texto):
-        if i == " ":
+        if i == " " or i == "end":
             break
         for j in range(len(i)):
             if (i[j]) == " " and marcador == 0:
@@ -19,13 +19,15 @@ def text_reader(texto):
                 auxiliar += [int(i[0:j])]
                 continue
             elif (i[j]) == " " and marcador == 1:
-                auxiliar += [int(i[j+1:(len(i)-1)])]
+                auxiliar += [int(i[j+1:(len(i)-1)] )]
                 marcador = 0
+        print(auxiliar)
         grafo +=[auxiliar]
         auxiliar = []
     return grafo
 def faz_tudo(texto):
     texto_lido = text_reader(texto)
+    print(texto_lido)
     matriz_final = matriz_adj(texto_lido)
     nedge = 0
     for i in range(len(matriz_final)-1): # só precisa ver até o vértice nvert-1, óbvio
@@ -47,7 +49,5 @@ def faz_tudo(texto):
                     
     return matriz_final, nedge, nvert, id_edge, arest
 #Formato do grafo tem que ser:
-path = str(input("Insira o grafo"))
-
-matriz_final, nedge, nvert, id_edge, arest = faz_tudo(open(path))
-
+# 1 linha (número de vértices), (número de arestas)
+# para cada linha, aresta: vérticeA - vérticeB, dessa exata maneira.
